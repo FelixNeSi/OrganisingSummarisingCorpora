1c4dd925ca9c7ea5086774729317543255f39bc7
Felix Needham-Simpson <felixnesi6@gmail.com>
2022-02-22 14:44:06 +0000

    BERT keyword extraction and gensim topic modelling clustering added

--- bert.py
+++ bert.py
@@ -1,8 +1,10 @@
 from preprocessing import *
+from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer, util
 from sklearn.cluster import KMeans, AgglomerativeClustering
 import numpy as np
-
+from sklearn.feature_extraction.text import CountVectorizer
+import itertools
 
 def create_transformer_model(model_name='distilbert-base-nli-mean-tokens'):
     model = SentenceTransformer(model_name)
@@ -30,7 +32,8 @@ def aglo_cluster(corpus_embeddings, n_clusters=None, distance_threshold=1.5):
 
 
 def fast_cluster(corpus_embeddings, min_community_size=25, threshold=0.75):
-    clusters = util.community_detection(corpus_embeddings, min_community_size=min_community_size, threshold=threshold, init_max_size=len(corpus_embeddings))
+    clusters = util.community_detection(corpus_embeddings, min_community_size=min_community_size, threshold=threshold,
+                                        init_max_size=len(corpus_embeddings))
     return clusters
 
 
@@ -53,20 +56,119 @@ def normalise_embeddings(embeddings):
     return embeddings
 
 
-data = ["This is a 1 TEST sentence that contains CaSes CaSes CaSes CaSes", "CaSes CaSes CaSes CaSes ALL OF THE STOPWORDS !@!@!@@", "CaSes CaSes Good TESTS go For EdGe CaSes"
-        , "Good TESTS go For EdGe CaSes CaSes CaSes CaSes", "Good TESTS go For EdGe CaSes CaSes CaSes CaSes"]
-model = create_transformer_model('distilbert-base-nli-mean-tokens')
-embeddings = encode_corpus_with_model(data, model)
-#embeddings = normalise_embeddings(embeddings)
-assignments = fast_cluster(embeddings, 3, 0.5)
-# for i, cluster in enumerate(clusters):
-#     print("\nCluster {}, #{} Elements ".format(i+1, len(cluster)))
-#     for sentence_id in cluster[0:3]:
-#         print("\t", corpus_sentences[sentence_id])
-#     print("\t", "...")
-#     for sentence_id in cluster[-3:]:
-#         print("\t", corpus_sentences[sentence_id])
-print(assignments)
-print_clusters(assignments, data)
+def generate_candidate_keywords(doc, min_ngram=1, max_ngram=1, remove_stopwords=True):
+    n_gram_range = (min_ngram, max_ngram)
+    stop_words = "english"
 
-# print(embeddings)
+    # Extract candidate words/phrases
+    if remove_stopwords:
+        count = CountVectorizer(ngram_range=n_gram_range, stop_words=stop_words).fit(doc)
+    else:
+        count = CountVectorizer(ngram_range=n_gram_range).fit(doc)
+    candidates = count.get_feature_names()
+    return candidates
+
+
+def get_most_cosine_similar(doc_embedding, candidate_embeddings, candidates, top_n=5):
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
+    return keywords
+
+
+def max_sum_sim(doc_embedding, candidate_embeddings, candidates, top_n, nr_candidates):
+    # Calculate distances and extract keywords
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    distances_candidates = cosine_similarity(candidate_embeddings,
+                                            candidate_embeddings)
+
+    # Get top_n words as candidates based on cosine similarity
+    words_idx = list(distances.argsort()[0][-nr_candidates:])
+    words_vals = [candidates[index] for index in words_idx]
+    distances_candidates = distances_candidates[np.ix_(words_idx, words_idx)]
+
+    # Calculate the combination of words that are the least similar to each other
+    min_sim = np.inf
+    candidate = None
+    for combination in itertools.combinations(range(len(words_idx)), top_n):
+        sim = sum([distances_candidates[i][j] for i in combination for j in combination if i != j])
+        if sim < min_sim:
+            candidate = combination
+            min_sim = sim
+
+    return [words_vals[idx] for idx in candidate]
+
+
+def mmr(doc_embedding, word_embeddings, candidates, top_n, diversity):
+
+    # Extract similarity within words, and between words and the document
+    word_doc_similarity = cosine_similarity(word_embeddings, doc_embedding)
+    word_similarity = cosine_similarity(word_embeddings)
+
+    # Initialize candidates and already choose best keyword/keyphras
+    keywords_idx = [np.argmax(word_doc_similarity)]
+    candidates_idx = [i for i in range(len(candidates)) if i != keywords_idx[0]]
+
+    for _ in range(top_n - 1):
+        # Extract similarities within candidates and
+        # between candidates and selected keywords/phrases
+        candidate_similarities = word_doc_similarity[candidates_idx, :]
+        target_similarities = np.max(word_similarity[candidates_idx][:, keywords_idx], axis=1)
+
+        # Calculate MMR
+        mmr = (1-diversity) * candidate_similarities - diversity * target_similarities.reshape(-1, 1)
+        mmr_idx = candidates_idx[np.argmax(mmr)]
+
+        # Update keywords & candidates
+        keywords_idx.append(mmr_idx)
+        candidates_idx.remove(mmr_idx)
+
+    return [candidates[idx] for idx in keywords_idx]
+
+
+def do_bert_keyword_extraction(data, sim_method="cosine", top_n=5, nr_candidates=10, diversity=0.2):
+    candidates = [generate_candidate_keywords([doc]) for doc in data]
+    model = create_transformer_model()
+    doc_embeddings = [model.encode([doc]) for doc in data]
+    candidate_embeddings = [model.encode(cand) for cand in candidates]
+    keywords = []
+    for i in range(len(data)):
+        if sim_method == "cosine":
+            keywords.append(get_most_cosine_similar(doc_embeddings[i], candidate_embeddings[i], candidates[i], top_n=top_n))
+        elif sim_method == "max_sum":
+            keywords.append(max_sum_sim(doc_embeddings[i], candidate_embeddings[i], candidates[i], top_n, nr_candidates))
+        elif sim_method == "max_marginal":
+            keywords.append(mmr(doc_embeddings[i], candidate_embeddings[i], candidates[i], top_n, diversity))
+    return keywords
+    # if sim_method == "cosine":
+    #     get_most_cosine_similar()
+    # print(candidates)
+    # print(len(doc_embeddings))
+    # print(len(candidate_embeddings))
+    # print(len(candidates))
+
+
+
+
+data = ["This is a 1 TEST sentence that contains CaSes CaSes CaSes CaSes",
+        "CaSes CaSes CaSes CaSes ALL OF THE STOPWORDS !@!@!@@", "CaSes CaSes Good TESTS go For EdGe CaSes"
+    , "Good TESTS go For EdGe CaSes CaSes CaSes CaSes", "Good TESTS go For EdGe CaSes CaSes CaSes CaSes"]
+
+# ['cases', 'contains', 'edge', 'good', 'sentence', 'stopwords', 'test', 'tests']
+
+# print(generate_candidate_keywords(data[0]))
+print(do_bert_keyword_extraction(data))
+# model = create_transformer_model('distilbert-base-nli-mean-tokens')
+# embeddings = encode_corpus_with_model(data, model)
+# # embeddings = normalise_embeddings(embeddings)
+# assignments = fast_cluster(embeddings, 3, 0.5)
+# # for i, cluster in enumerate(clusters):
+# #     print("\nCluster {}, #{} Elements ".format(i+1, len(cluster)))
+# #     for sentence_id in cluster[0:3]:
+# #         print("\t", corpus_sentences[sentence_id])
+# #     print("\t", "...")
+# #     for sentence_id in cluster[-3:]:
+# #         print("\t", corpus_sentences[sentence_id])
+# print(assignments)
+# print_clusters(assignments, data)
+#
+# # print(embeddings)
--- topic_modelling.py
+++ topic_modelling.py
@@ -2,45 +2,58 @@ import pyLDAvis
 import pyLDAvis.gensim_models as gensimvis
 from gensim.models import Phrases
 from gensim.corpora import Dictionary
-from gensim.models import LdaModel
+from gensim.models import LdaModel, LsiModel
 
 
-def LDAvis(documents, file_name, num_of_topics=100):
+def prep_docs_for_topic_modelling(documents):
+    documents = [doc.split() for doc in documents]
     lda_dictionary = Dictionary(documents)
-
     lda_corpus = [lda_dictionary.doc2bow(doc) for doc in documents]
-
-    print('Number of unique tokens: %d' % len(lda_dictionary))
-    print('Number of documents: %d' % len(lda_corpus))
-
-    num_topics = num_of_topics
-    chunksize = 2000
-    passes = 10
-    iterations = 400
-    eval_every = None
-
     temp = lda_dictionary[0]
     id2word = lda_dictionary.id2token
 
+    return lda_corpus, id2word
+
+
+def lda(lda_corpus, id2word, num_topics=100, chunksize=2000, passes=10, iterations=400, eval_every=None, alpha='auto',
+        eta='auto', minimum_probability=0.0):
     model = LdaModel(
         corpus=lda_corpus,
         id2word=id2word,
         chunksize=chunksize,
-        alpha='auto',
-        eta='auto',
+        alpha=alpha,
+        eta=eta,
         iterations=iterations,
         num_topics=num_topics,
         passes=passes,
-        eval_every=eval_every
+        eval_every=eval_every,
+        minimum_probability=minimum_probability
     )
 
-    top_topics = model.top_topics(lda_corpus)
+    return model
 
-    from pprint import pprint
-    pprint(top_topics)
 
-    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-    print('Average topic coherence: %.4f.' % avg_topic_coherence)
+def get_topic_distributions(model, lda_corpus):
+    topic_distributions = [[d[1] for d in dist] for dist in [model[text] for text in lda_corpus]]
+    # only_topic_dist = [[d[1] for d in dist] for dist in topic_distributions]
+    return topic_distributions
 
-    vis_data = gensimvis.prepare(model, lda_corpus, lda_dictionary)
-    pyLDAvis.save_html(vis_data, file_name)
+
+def do_get_topic_model(data):
+    lda_corp, id2Word = prep_docs_for_topic_modelling(data)
+    model = lda(lda_corp, id2Word, num_topics=2)
+    topic_dist = get_topic_distributions(model, lda_corp)
+    return topic_dist
+
+
+data = ["This is a 1 TEST sentence that contains CaSes CaSes CaSes CaSes", "CaSes CaSes CaSes CaSes ALL OF THE STOPWORDS !@!@!@@", "CaSes CaSes Good TESTS go For EdGe CaSes"
+        , "Good TESTS go For EdGe CaSes CaSes CaSes CaSes", "Good TESTS go For EdGe CaSes CaSes CaSes CaSes"]
+
+lda_corp, id2Word = prep_docs_for_topic_modelling(data)
+model = lda(lda_corp, id2Word, num_topics=2)
+x = get_topic_distributions(model, lda_corp)
+print(x)
+
+print(do_get_topic_model(data))
+
+
